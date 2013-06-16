4708b952af4dac51a2f3f3cee4a8f8f1922eb9d3
Endoze <endoze@endozemedia.com>
2013-06-15 23:59:14 -0400

    Remove nike client from core
    This commit refactors the core class to move everything out into it's
    own file and include it here.

--- lib/nike_2_runkeeper/core.rb
+++ lib/nike_2_runkeeper/core.rb
@@ -1,24 +1,9 @@
 require 'nike_2_runkeeper/version'
+require 'nike_2_runkeeper/nike_client'
+require 'nike_2_runkeeper/runkeeper_client'
 require 'nike'
 require 'runkeeper'
 
 module Nike2Runkeeper
-  class NikeClient
-    attr_accessor :activities, :client
 
-    def initialize(username, password)
-      @activities = Array.new
-      @client = Nike::Client.new(username, password)
-    end
-
-    def get_activities
-      return @activities if @activities
-
-      nike_activities = @client.activities
-      nike_activities.each do |a|
-        @activities << @client.activity(a.activity_id)
-      end
-    end
-
-  end
 end
