43936bce41c7b868fe4a4ed1f717b61c4b97838d
Endoze <endoze@endozemedia.com>
2013-06-16 18:00:59 -0400

    Switch runkeeper dependency to health graph
    This commit switches the runkeeper dependency over to health graph
    instead.

--- lib/nike_2_runkeeper/core.rb
+++ lib/nike_2_runkeeper/core.rb
@@ -2,7 +2,7 @@ require 'nike_2_runkeeper/version'
 require 'nike_2_runkeeper/nike_client'
 require 'nike_2_runkeeper/runkeeper_client'
 require 'nike'
-require 'runkeeper'
+require 'health_graph'
 
 module Nike2Runkeeper
 
